a36587858ed8d353b82419e60d3be56936759183
winding902 <146059255+winding902@users.noreply.github.com>
2023-12-21 19:21:44 +0900

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,16 @@
 <?php
 
-$link = mysqli_connect("localhost", "cf619105_zyozai", "cf619105", "cf619105_database");
+$link = mysqli_connect("localhost", "", "", "");
 
 $TOKEN = '';
 
 
-$ClientId = '1119408359582482542';
-$ClientSecret='B2usYAtDuse42o1zPvmwgYIQwAamUjnD';
+$ClientId = '';
+$ClientSecret='';
 
-$RedirectURI = 'https://cf619105.cloudfree.jp/verify';
-$Endpoint = 'https://discord.com/api/oauth2/authorize?client_id=1119408359582482542&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Fcf619105.cloudfree.jp%2Fverify&scope=bot+identify+guilds.join';
-
-$NICOKey = 'ilventodoro';
+$RedirectURI = '';
+$Endpoint = '';
+$NICOKey = '';
 
 
 ?>

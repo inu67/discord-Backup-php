c20b2c2d08d186cc5aeb78eae94e9a78b96421b2
winding902 <146059255+winding902@users.noreply.github.com>
2023-12-21 19:33:52 +0900

    Update index.php

--- verify/index.php
+++ verify/index.php
@@ -60,7 +60,7 @@ if(get('code')){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="貴様見ているな！？">
+	<meta name="description" content="！？">
     <title>NICO-Verify</title>
 	<link rel="stylesheet" href="./verify.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"/>
